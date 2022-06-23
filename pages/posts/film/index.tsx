8d8af826b3d3019b9642f5e5cb6ee815770d89c2
brianreidy <brianreidy3@gmail.com>
2022-06-23 17:02:14 -0400

    feat: film images for june 2022

---
+++ pages/posts/film/index.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  max-height: 100vh;
+  margin: 0 auto;
+`;
+
+const range = (start: number, end: number) => {
+  const length = end - start;
+  return Array.from({ length }, (_, i) => start + i);
+};
+export default function Home() {
+  return (
+    <Container>
+      {range(1, 38).map((i) => (
+        <Image key={i} src={`/static/images/2022_June_B&W_New_York/${i}.JPG`} />
+      ))}
+    </Container>
+  );
+}
