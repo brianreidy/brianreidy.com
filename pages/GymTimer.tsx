fdb5e20b6ee0f60d3ddf868db42d44929636949e
Brian Reidy <brianreidy3@gmail.com>
2023-10-19 19:17:13 -0400

    feat: GymTimer project

---
+++ pages/GymTimer.tsx
@@ -0,0 +1,159 @@
+import Head from 'next/head';
+import styled from '@emotion/styled';
+import { useState, useEffect, useRef } from 'react';
+import FormControl from '@mui/material/FormControl';
+import {
+  Button,
+  ButtonGroup,
+  CircularProgress,
+  Container,
+} from '@mui/material';
+
+import colors from '@src/lib/colors';
+
+const Wrapper = styled.div`
+  padding-top: 1em;
+  padding-bottom: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.background.primary};
+  height: 100vh;
+`;
+const CenterRail = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px; 0px;
+  flex-direction: column;
+  height:100%;
+`;
+
+const useTimer = (
+  def: number | undefined,
+): [number | undefined, (n: number | undefined) => void] => {
+  const [timer, setTimer] = useState<number | undefined>(def);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => (prevTimer ? prevTimer - 1 : prevTimer));
+    }, 1000);
+
+    if (timer === 0) {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [timer]);
+
+  return [timer, setTimer];
+};
+
+type WorkoutType = 'main' | 'accessory';
+
+const getProgress = (
+  workoutType: WorkoutType | undefined,
+  timer: number,
+): number => {
+  if (workoutType === 'main') {
+    return (timer / (4 * 60)) * 100;
+  } else if (workoutType === 'accessory') {
+    return (timer / (2 * 60)) * 100;
+  }
+  return 0;
+};
+
+export default function GymTimer() {
+  const [timer, setTimer] = useTimer(undefined);
+  const [workoutType, setWorkoutType] = useState<WorkoutType | undefined>();
+  const audioRef = useRef<HTMLAudioElement | null>(null); // Create a ref for the Audio element
+
+  useEffect(() => {
+    console.log(timer);
+    if (timer) console.log(getProgress(workoutType, timer));
+
+    if (timer === 0 && audioRef?.current !== null) {
+      console.log('play');
+      audioRef.current.play();
+    }
+  }, [timer, workoutType]);
+
+  const handleFinishLiftClick = (workOutType: WorkoutType) => {
+    if (workOutType === 'main') {
+      setTimer(4 * 60);
+    } else {
+      setTimer(2 * 60);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Head>
+        <title>brian reidy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <audio ref={audioRef}>
+        <source src="/cowBells.wav" type="audio/wav" />
+      </audio>
+      <CenterRail maxWidth="md">
+        <CircularProgress
+          variant="determinate"
+          value={timer ? getProgress(workoutType, timer) : 0}
+          size={'70%'}
+          sx={{ display: 'flex' }}
+        />
+        {workoutType === undefined ? (
+          <FormControl fullWidth>
+            <ButtonGroup
+              sx={{ pt: 2 }}
+              orientation="vertical"
+              aria-label="vertical outlined button group"
+            >
+              <Button
+                key="main"
+                sx={{ p: 2 }}
+                onClick={() => setWorkoutType('main')}
+              >
+                Main Lift (4 min)
+              </Button>
+              <Button
+                key="accessory"
+                sx={{ p: 2 }}
+                onClick={() => setWorkoutType('accessory')}
+              >
+                Accessory Lift (2 min)
+              </Button>
+            </ButtonGroup>
+          </FormControl>
+        ) : (
+          <FormControl fullWidth>
+            <ButtonGroup
+              orientation="vertical"
+              aria-label="vertical contained button group"
+              variant="contained"
+            >
+              <Button
+                key="main"
+                sx={{ p: 2 }}
+                onClick={() => handleFinishLiftClick(workoutType)}
+              >
+                Start Rest
+              </Button>
+              <Button
+                key="accessory"
+                sx={{ p: 2 }}
+                onClick={() => {
+                  setWorkoutType(undefined);
+                  setTimer(undefined);
+                }}
+              >
+                Back
+              </Button>
+            </ButtonGroup>
+          </FormControl>
+        )}
+      </CenterRail>
+    </Wrapper>
+  );
+}
---
+++ posts/gym_timer.md
@@ -0,0 +1,16 @@
+---
+title: I made a simple gym timer
+date: 2023/10/19
+---
+
+Project: [https://brianreidy.com/GymTimer](/GymTimer)
+
+Code:[https://github.com/brianreidy/brianreidy.com/pull/24](https://github.com/brianreidy/brianreidy.com/pull/24)
+
+I created a simple gym rest timer inspired by the article ["Write More Useless Software"](https://ntietz.com/blog/write-more-useless-software/). The goal was to have some fun while also learning new things.
+
+When weightlifting, I noticed that for the big lifts like bench press, squat, and deadlift, I needed about 5 minutes to recover in between sets. However, for smaller accessory lifts like close grip bench, bicep curls, and chin-ups, I only needed about 2 minutes to recover.
+
+To address this, I developed a gym rest timer that automatically adjusts the rest time based on the type of exercise. It allows me to set different rest durations for the big lifts and the accessory lifts, ensuring that I optimize my workout efficiency.
+
+This gym timer is pretty simple but i did learn a little bit about audio along the way. This uses a simple html audio element but while looking into it I learned about [https://howlerjs.com/](https://howlerjs.com/) and all of the features that I could use in the future.
